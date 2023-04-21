0e5ad430eebc99c1e580668388fd8365ce0da72e
Cathal O'Connor <catoconn@redhat.com>
2023-04-21 15:13:16 +0000

    Added Accounts Endpoints

---
+++ src/config/dependencies.js
@@ -0,0 +1,20 @@
+import AccountsRepositoryInMemory from '../accounts/repositories/InMemoryRepository';
+
+const buildDependencies = () => {
+  const dependencies = {
+  };
+
+  console.log("DB "+ process.env.DATABASE_DIALECT)
+  if (process.env.DATABASE_DIALECT === "in-memory") {
+    dependencies.accountsRepository = new AccountsRepositoryInMemory();
+  } else if (process.env.DATABASE_DIALECT === "mongo") {
+    throw new Error('Add Mongo Support');
+  } else if (process.env.DATABASE_DIALECT === "mysql") {
+    throw new Error('Add MySQL support');
+  } else {
+    throw new Error('Add DB Support to project');
+  }
+  return dependencies;
+};
+
+export default buildDependencies;
