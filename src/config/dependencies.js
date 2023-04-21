6962b991bc67759adc9900ec66a1d2e7fac0974f
Cathal O'Connor <catoconn@redhat.com>
2023-04-21 22:10:01 +0000

    Update mongodb connection

--- src/config/dependencies.js
+++ src/config/dependencies.js
@@ -1,4 +1,5 @@
 import AccountsRepositoryInMemory from '../accounts/repositories/InMemoryRepository';
+import AccountsRepositoryMongo from '../accounts/repositories/MongoAccountRepository';
 
 const buildDependencies = () => {
   const dependencies = {
@@ -8,7 +9,7 @@ const buildDependencies = () => {
   if (process.env.DATABASE_DIALECT === "in-memory") {
     dependencies.accountsRepository = new AccountsRepositoryInMemory();
   } else if (process.env.DATABASE_DIALECT === "mongo") {
-    
+    dependencies.accountsRepository = new AccountsRepositoryMongo();
   } else if (process.env.DATABASE_DIALECT === "mysql") {
     throw new Error('Add MySQL support');
   } else {
