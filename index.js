3ee4615d6c8a75a09577421c4cbaa1253f523d70
Cathal O'Connor <catoconn@redhat.com>
2023-04-21 21:43:21 +0000

    mongodb

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ import genresRouter from './src/genres';
 import createAccountsRouter from './src/accounts/routes';
 import buildDependencies from "./src/config/dependencies";
 import createMoviesRouter from './src/movies/routes';
+import db from './src/config/db';
 
 dotenv.config();
+db.init(); //add BELOW dotenv.config();
 const dependencies = buildDependencies();
 
 const app = express();
